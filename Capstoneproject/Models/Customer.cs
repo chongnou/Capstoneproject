220ac05a7c729b7944b45c2c6599b484a60988e4
chong <39102393+chongnou@users.noreply.github.com>
2018-07-23 22:30:32 -0500

    added pk/fk to admin/customer models

--- Capstoneproject/Models/Customer.cs
+++ Capstoneproject/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace Capstoneproject.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Customer Name")]
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserID { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
--- Capstoneproject/Models/EventList.cs
+++ Capstoneproject/Models/EventList.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,12 @@ namespace Capstoneproject.Models
 {
     public class EventList
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hours { get; set; }
+        public string Cost { get; set; }
+        public string address { get; set; }
+
     }
 }
