4d8b590d2a521358b819bb8557f439e779e2f155
chong <39102393+chongnou@users.noreply.github.com>
2018-07-23 22:32:56 -0500

    added closetime model

---
+++ Capstoneproject/Models/CloseTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Capstoneproject.Models
+{
+    public class CloseTime
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Closing Time")]
+        public string Start { get; set; }
+    }
+}
