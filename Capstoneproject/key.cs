983674e2e31698622fb93716c54fcbab1604d966
chong <39102393+chongnou@users.noreply.github.com>
2018-07-26 10:26:21 -0500

    key private

---
+++ Capstoneproject/key.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstoneproject
+{
+    public partial class key
+    {
+        private const string SecretKey = "WRITE_HERE_YOUR_API_Key";
+    }
+}
